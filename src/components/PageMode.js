c777cc72bc33545d1d6c91252e59eece11aa67c0
João Gabriel <jgabrielfes@gmail.com>
2022-02-23 10:35:14 -0300

    Dark/Light mode ajustado :rocket:

--- src/components/PageMode.js
+++ src/components/PageMode.js
@@ -1,42 +1,36 @@
 import React from 'react';
-import { CgDarkMode } from 'react-icons/cg';
-
-const pageMode = localStorage.pageMode || 'light';
-
-const pageModeColors = {
-  light: {
-    bgColor: 'white',
-    headerColor: 'white',
-  },
-  dark: {
-    bgColor: 'rgb(32, 32, 32)',
-    headerColor: 'rgb(96, 96, 96)',
-  }
-}
+import LightMode from '@mui/icons-material/LightMode';
+import Switch from '@mui/material/Switch';
+import DarkMode from '@mui/icons-material/DarkMode';
+import pageModeColors from './PageModeData';
 
 class PageMode extends React.Component {
   constructor() {
     super();
     this.state = {
-      pageMode: pageMode
+      pageMode: localStorage.pageMode || 'light',
     };
     this.handlePageMode = this.handlePageMode.bind(this);
   }
 
   handlePageMode() {
-    const { pageMode } = this.state;
     this.setState({
-      pageMode: pageMode === 'light' ? 'dark' : 'light'
-    }, () => localStorage.pageMode = pageMode);
-    for (let key of Object.keys(pageModeColors[pageMode])) {
-      document.querySelector(':root').style.setProperty(`--${key}`, pageModeColors[pageMode][key]);
-    }
+      pageMode: this.state.pageMode === 'light' ? 'dark' : 'light'
+    }, () => localStorage.pageMode = this.state.pageMode);
   }
 
   render() {
+    const { pageMode } = this.state;
+
+    for (let key of Object.keys(pageModeColors[pageMode])) {
+      document.querySelector(':root').style.setProperty(`--${key}`, pageModeColors[pageMode][key]);
+    }
+
     return (
       <button className="page-mode-btn" onClick={this.handlePageMode}>
-        <CgDarkMode size="1.8em" />
+        <LightMode />
+        <Switch checked={pageMode === 'dark'} color="default" />
+        <DarkMode />
       </button>
     )
   }
---
+++ src/components/PageModeData.js
@@ -0,0 +1,20 @@
+const pageModeColors = {
+  light: {
+    bgColor: 'white',
+    darkIconColor: 'black',
+    fontColor: 'black',
+    headerColor: 'rgb(192, 192, 192)',
+    lightIconColor: 'yellow',
+    shadowColor: 'gray',
+  },
+  dark: {
+    bgColor: 'rgb(32, 32, 32)',
+    darkIconColor: 'purple',
+    fontColor: 'white',
+    headerColor: 'rgb(64, 64, 64)',
+    lightIconColor: 'white',
+    shadowColor: 'black',
+  }
+}
+
+export default pageModeColors;
